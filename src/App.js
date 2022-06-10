6a3db13ada2968d2204ecfe4ef23de2621991cf6
AbdSh <ashmaitelly@gmail.com>
2022-06-10 20:22:53 +0300

    Added google login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import './App.css';
+import React from 'react';
+import { useEffect } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const callbackResponse = (response) => {
+    console.log(response.credential);
+  };
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: process.env.CLIENT_ID,
+      callback: callbackResponse,
+    });
+    google.accounts.id.renderButton(document.getElementById('google-signin'), {
+      theme: 'outline',
+      size: 'large',
+    });
+  }, []);
+  return (
+    <div className="App">
+      <div id="google-signin" />
+    </div>
+  );
 }
 
 export default App;
