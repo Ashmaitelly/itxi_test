6e955fc12f4c7a5c20add8401c1dca3ede5032b8
AbdSh <ashmaitelly@gmail.com>
2022-06-11 20:12:48 +0300

    Installed MUI

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { useEffect } from 'react';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const SignIn = () => {
+const theme = createTheme();
+
+export default function SignIn() {
   const callbackResponse = (response) => {
     console.log(response.credential);
   };
 
-  useEffect(() => {
+  React.useEffect(() => {
     /* global google */
     google.accounts.id.initialize({
-      client_id: process.env.CLIENT_ID,
+      client_id:
+        '943775489068-1t3qu1qjrchtgbontkako5u51020ofid.apps.googleusercontent.com',
       callback: callbackResponse,
     });
     google.accounts.id.renderButton(document.getElementById('google-signin'), {
@@ -18,10 +25,21 @@ const SignIn = () => {
     });
   }, []);
   return (
-    <div>
-      <div id="google-signin" />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}></Avatar>
+          <div id="google-signin" />
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
-};
-
-export default SignIn;
+}
