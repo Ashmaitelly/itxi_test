b356733f9ea9eb5e07d18cec5d0c5ac0313dc427
AbdSh <ashmaitelly@gmail.com>
2022-06-20 11:41:19 +0300

    Fixed some styling

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -14,20 +14,18 @@ const BookCard = ({ data }) => {
         component="img"
         height="190"
         width="160"
-        image={volumeInfo.imageLinks.smallThumbnail}
+        image={volumeInfo.imageLinks.thumbnail}
         alt="green iguana"
       />
       <CardContent>
-        <Typography gutterBottom component="div">
+        <Typography gutterBottom variant="h5" component="div">
           {title.length < 50 ? title : title.substring(0, 50) + '...'}{' '}
           {`(${volumeInfo.publishedDate})`}
         </Typography>
         <Typography gutterBottom component="div">
           by {volumeInfo.authors}
         </Typography>
-        <Typography gutterBottom component="div">
-          {volumeInfo.publisher}
-        </Typography>
+        <Typography component="div">{volumeInfo.publisher}</Typography>
       </CardContent>
     </Card>
   );
