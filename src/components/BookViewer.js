88c2c89ae35361cd5bcaa3d4dea38322c08e4956
AbdSh <ashmaitelly@gmail.com>
2022-06-22 20:03:38 +0300

    Embedded viewer works

--- src/components/BookViewer.js
+++ src/components/BookViewer.js
@@ -1,48 +1,11 @@
 import React, { useEffect, useState, useRef } from 'react';
 
 const BookViewer = ({ id }) => {
-  // Initialize loaded state as false
-  const [loaded, setLoaded] = useState(false);
-  const canvasRef = useRef();
-  // Create alert message if book not found in Google Database
-  function alertNotFound() {
-    alert('could not embed the book!');
-  }
-  // Add a Google Books script tag and event listener if the tag has loaded
-  useEffect(() => {
-    const scriptTag = document.createElement('script');
-    scriptTag.src = 'https://www.google.com/books/jsapi.js';
-    scriptTag.addEventListener('load', () => setLoaded(true));
-    scriptTag.id = 'google-script';
-    document.body.appendChild(scriptTag);
-  }, []);
-  useEffect(() => {
-    if (!loaded) return;
-    else {
-      if (window.viewer) {
-        let viewer = new window.google.books.DefaultViewer(canvasRef.current);
-        viewer.load(id, alertNotFound);
-      } else {
-        window.google.books.load();
-        window.google.books.setOnLoadCallback(() => {
-          let viewer = new window.google.books.DefaultViewer(canvasRef.current);
-          window.viewer = viewer;
-          viewer.load(id, alertNotFound);
-        });
-      }
-    }
-  }, [loaded]);
-
   return (
-    <div>
-      {loaded ? (
-        <div>
-          <div ref={canvasRef} id="viewerCanvas"></div>
-        </div>
-      ) : (
-        'Script not loaded'
-      )}
-    </div>
+    <iframe
+      src={`https://books.google.com.lb/books?id=${id}&pg=PP5&output=embed`}
+      style={{ height: '70vh' }}
+    ></iframe>
   );
 };
 
